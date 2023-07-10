0d60242ae5d37283d5328b599795ed2d2fdf781c
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2023-07-10 16:56:46 +0900

    a

--- TR1/Bezier.cpp
+++ TR1/Bezier.cpp
@@ -98,10 +98,7 @@ void Bezier::Init() {
 }
 
 void Bezier::Update() {
-	if (Novice::CheckHitKey(DIK_UP) || Novice::CheckHitKey(DIK_DOWN) || Novice::CheckHitKey(DIK_LEFT) || Novice::CheckHitKey(DIK_RIGHT)) {
-		Move();
-	}
-	
+	Move();
 }
 
 void Bezier::Move() {
